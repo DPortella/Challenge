e6ea0a3f70df9b262a336c40b797256423140e11
DPortella <d.portellaaraujo@gmail.com>
2024-03-09 02:01:54 -0300

    Atualizando

---
+++ pymupdf-venv/Lib/site-packages/fitz/__init__.py
@@ -0,0 +1,11 @@
+from frontend import *
+import tools
+import os.path as op
+
+__version__ = '0.0.1dev2'
+__license__ = 'BSD-3'
+__author__ = 'Erik Kastman'
+__author_email__ = 'erik.kastman@gmail.com'
+__maintainer_email__ = 'erik.kastman@gmail.com'
+__url__ = 'https://github.com/kastman/fitz'
+__downloadUrl__ = 'https://github.com/kastman/fitz/releases'
